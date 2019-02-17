0f1095dc1fe0b2c8d40a015ebe6d6b5da4c85dde
Isaac Pierce <contact@isaacdpierce.com>
2019-02-16 17:27:26 -0800

    Upgrade styles add gifs and restart quiz logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 // TODO Check course requirements to make sure all is done.
-//////////////////////////////////////////////////////
+// TODO Show Correct answer when wrong answer is chosen
+// TODO Remove score from bottom of correct popup
+////////////////////////////////////////////////////////
 // HTML PRODUCTION FUNCTIONS ////////////////////////
 ////////////////////////////////////////////////////
 
@@ -26,60 +28,64 @@ function makeForm() {
   } = renderNextQuestion();
 
   return `
-  <form class="quiz__form">
-  <h3>#${number}. ${question}</h3>
-  <ul>
-    <li>
-      <label for="answer1">
-        <input
-          type="radio"
-          id="answer1"
-          class="js-answer1 answer1"
-          name="answer"
-          value=""
-        />
-        ${answer1}
-      </label>
-    </li>
-    <li>
-      <label for="answer2">
-        <input
-          type="radio"
-          id="answer2"
-          class="js-answer2 answer2"
-          name="answer"
-          value=""
-        />
-        ${answer2}
-      </label>
-    </li>
-    <li>
-      <label for="answer3">
-        <input
-          type="radio"
-          id="answer3"
-          class="js-answer3 answer3"
-          name="answer"
-          value=""
-        />
-        ${answer3}
-      </label>
-    </li>
-    <li>
-      <label for="answer4">
-        <input
-          type="radio"
-          id="answer4"
-          class="js-answer4 answer4"
-          name="answer"
-          value=""
-        />
-        ${answer4}
-      </label>
-    </li>
-  </ul>
-  <button class="btn__question btn__question-submit">Submit Answer</button>
-</form>
+    <form class="quiz__form">
+      <header>
+        <span class="number">#${number}.</span>
+        <p><span class="question">${question}</span></p>
+      </header>
+      <ul>
+        <li>
+          <label for="answer1">
+            <input
+              type="radio"
+              id="answer1"
+              class="js-answer1 answer1"
+              name="answer"
+              value=""
+              required
+            />
+            ${answer1}
+          </label>
+        </li>
+        <li>
+          <label for="answer2">
+            <input
+              type="radio"
+              id="answer2"
+              class="js-answer2 answer2"
+              name="answer"
+              value=""
+            />
+            ${answer2}
+          </label>
+        </li>
+        <li>
+          <label for="answer3">
+            <input
+              type="radio"
+              id="answer3"
+              class="js-answer3 answer3"
+              name="answer"
+              value=""
+            />
+            ${answer3}
+          </label>
+        </li>
+        <li>
+          <label for="answer4">
+            <input
+              type="radio"
+              id="answer4"
+              class="js-answer4 answer4"
+              name="answer"
+              value=""
+            />
+            ${answer4}
+          </label>
+        </li>
+      </ul>
+      <button class="btn__question btn__question-submit">Submit Answer</button>
+    </form>
   `;
 }
 
@@ -96,14 +102,19 @@ function makeScoreCard() {
 }
 
 function makeWrongPopup() {
+  let nextQuestion = renderNextQuestion();
+  let correctAnswer = nextQuestion.correctAnswer;
   return `
     <aside class='js-popup__response popup__response'>
-      <div class='popup__text popup__bg-wrong'>
-        <div>
-        <h1>Incorrect</h1>
+      <header class='popup__text popup__bg-wrong'>
+        <hgroup>
+          <h1>Incorrect</h1>
+          <h2>The correct response was:</h2>
+          <p class="js-popup__response--correct"></p>
           <button class="js-btn__question-next btn__question btn__question-next">Next \u{27A1}</button>
-        </div>
-      </div>
+        </hgroup>
+        
+      </header>
     </aside>
   `;
 }
@@ -114,8 +125,8 @@ function makeRightPopup() {
       <div class='popup__text popup__bg-correct'>
         <div>
           <h1>Correct</h1>
+          <p>1 step closer to SVG badassery</p>
           <button class="js-btn__question-next btn__question btn__question-next">Next \u{27A1}</button>
-          <h2>Score: ${STORE.rightAnswers} out of 10</h2>
         </div>
       </div>
     </aside>
@@ -129,7 +140,7 @@ function makeQuizOverPopup() {
         <div>
           <h1>Quiz Complete</h1>
           <h2>You got ${STORE.rightAnswers} out 10</h2>
-          <p>You should be <span class="feedback"></span> of yourself!</p>
+          <p>You oughta be <span class="feedback"></span> of yourself!</p>
           <button class="js-btn__restart btn__question btn__question-next">Restart Quiz \u{27A1}</button>
         </div>
       </div>
@@ -165,10 +176,12 @@ function renderNextQuestion() {
 
 function renderCorrectResponse() {
   STORE.rightAnswers += 1;
+  removeRecentQuestion();
   renderScoreCard();
   $('main').append(makeRightPopup);
 }
 function renderWrongResponse() {
+  removeRecentQuestion();
   $('main').append(makeWrongPopup);
 }
 
@@ -180,12 +193,10 @@ function renderQuizOverPopup() {
 function renderFeedback() {
   totalRight = STORE.rightAnswers;
   if (totalRight < 8) {
-    // TODO /////////////////////////////////////////////////////
-    // Get image to add to background
-    $('.popup__text').addClass('.popup__bg-shame');
+    $('.popup__text').addClass('popup__bg-shame');
     $('.feedback').append('ashamed');
   } else {
-    $('.popup__text').addClass('.popup__bg-praise');
+    $('.popup__text').addClass('popup__bg-praise');
     $('.feedback').append('proud');
   }
 }
@@ -202,11 +213,20 @@ function removePopup() {
   if ('js-popup__response') {
     $('.js-popup__response').remove();
   }
+  if ('js-popup__response-over') {
+    $('.js-popup__response-over').remove();
+  }
 }
 
 function removeRecentQuestion() {
   if ($('.quiz__form')) {
     $('.quiz__form').remove();
+  }
+}
+
+function removeScoreCard() {
+  if ($('.card__score')) {
+    $('.card__score').remove();
   }
 }
 
@@ -225,7 +245,10 @@ function handleStartQuiz() {
 function handleSubmitAnswer() {
   // Onsubmit get correct answer from STORE
   $('.js-quiz__container').on('submit', 'form', function(event) {
-    let answer = STORE.questionBlocks[STORE.questionCount - 1].correctAnswer;
+    let currentQuestion = STORE.questionBlocks[STORE.questionCount - 1];
+    let answer = currentQuestion.correctAnswer;
+    let answerText = currentQuestion[answer];
+
     event.preventDefault();
 
     // check if answer is same as the checked input id
@@ -233,15 +256,31 @@ function handleSubmitAnswer() {
       renderCorrectResponse();
     } else {
       renderWrongResponse();
+      $('.js-popup__response--correct').append(answerText);
     }
   });
 }
 
+function formReset() {
+  removePopup();
+  removeRecentQuestion();
+  renderForm();
+}
+
 function handleNextQuestionButton() {
   $('main').on('click', '.js-btn__question-next', function(event) {
+    formReset();
+  });
+}
+
+function handleRestartQuiz() {
+  $('main').on('click', '.js-btn__restart', function(event) {
+    STORE.questionCount = 0;
+    STORE.rightAnswers = 0;
     removePopup();
     removeRecentQuestion();
-    renderForm();
+    removeScoreCard();
+    renderStartPage();
   });
 }
 
@@ -250,6 +289,7 @@ function runQuiz() {
   handleStartQuiz();
   handleSubmitAnswer();
   handleNextQuestionButton();
+  handleRestartQuiz();
 }
 
 $(runQuiz);
